c2980b1eab70202b09d31db7cdce317b1c05c2b2
Andrew Druk <adruk@readdle.com>
2023-10-19 16:57:49 +0300

    Dev: update java_swift

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
         )
     ],
     dependencies: [
-        .package(url: "https://github.com/readdle/java_swift.git", .upToNextMinor(from: "2.1.5")),
+        .package(url: "https://github.com/readdle/java_swift.git", .upToNextMinor(from: "2.2.0")),
         .package(url: "https://github.com/readdle/swift-anycodable.git", .upToNextMinor(from: "1.0.2")),
     ],
     targets: [
